ddcdeaa1882989f3dc32e53b25144ff7134e6c13
abijithnv <abijithnv@gmail.com>
2024-03-29 13:38:30 +0530

    started movie section designing

---
+++ script.js
@@ -0,0 +1,59 @@
+  let a=[
+            {
+                name:"Avengers",
+                rating:8,
+                cover:"https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
+            },
+            {
+                name:"The Dark Knight",
+                rating:9,
+                cover:"https://contentserver.com.au/assets/598411_p173378_p_v8_au.jpg"
+            },
+            {
+                name:"Venom",
+                rating:6.6,
+                cover:"https://m.media-amazon.com/images/M/MV5BNTFkZjdjN2QtOGE5MS00ZTgzLTgxZjAtYzkyZWQ5MjEzYmZjXkEyXkFqcGdeQXVyMTM0NTUzNDIy._V1_.jpg"
+            },
+            {
+                name:"Jumanji",
+                rating:7.1,
+                cover:"https://m.media-amazon.com/images/S/pv-target-images/6605eddeb3e44a153694c675cd1129d2a9d7d25576366657536f7311d9d7dc99.jpg"
+            },
+            {
+                name:"Spider Man",
+                rating:7.4,
+                cover:"https://moviesmedia.ign.com/movies/image/object/033/033717/spider-man_poster.jpg"
+            },
+            {
+                name:"Avatar",
+                rating:7.9,
+                cover:"https://lumiere-a.akamaihd.net/v1/images/avatar_800x1200_208c9665.jpeg?region=0%2C0%2C800%2C1200"
+            },
+            {
+                name:"Dune",
+                rating:8.0,
+                cover:"https://m.media-amazon.com/images/M/MV5BMDQ0NjgyN2YtNWViNS00YjA3LTkxNDktYzFkZTExZGMxZDkxXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_FMjpg_UX1000_.jpg"
+            },
+            {
+                name:"Godzilla",
+                rating:6.0,
+                cover:"https://m.media-amazon.com/images/M/MV5BOGFjYWNkMTMtMTg1ZC00Y2I4LTg0ZTYtN2ZlMzI4MGQwNzg4XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg"
+            },
+            {
+                name:"The conjuring",
+                rating:7.5,
+                cover:"https://m.media-amazon.com/images/M/MV5BMTM3NjA1NDMyMV5BMl5BanBnXkFtZTcwMDQzNDMzOQ@@._V1_.jpg"
+            },
+            {
+                name:"Free Guy",
+                rating:7.1,
+                cover:"https://m.media-amazon.com/images/M/MV5BOTY2NzFjODctOWUzMC00MGZhLTlhNjMtM2Y2ODBiNGY1ZWRiXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg"
+            },
+
+        ];
+
+function searchMovie()
+{
+    let movieName=document.getElementById("search").value;
+    console.log(movieName);
+}
