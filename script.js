a33ee2b963d2e603c04dd5680b50f43e7648f58b
abijithnv <abijithnv@gmail.com>
2024-04-09 14:27:35 +0530

    update js

--- script.js
+++ script.js
@@ -3,53 +3,138 @@
                 name:"Avengers",
                 rating:8,
                 cover:"https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
-            },
-            {
-                name:"The Dark Knight",
-                rating:9,
-                cover:"https://contentserver.com.au/assets/598411_p173378_p_v8_au.jpg",
-            },
-            {
-                name:"Venom",
-                rating:6.6,
-                cover:"https://m.media-amazon.com/images/M/MV5BNTFkZjdjN2QtOGE5MS00ZTgzLTgxZjAtYzkyZWQ5MjEzYmZjXkEyXkFqcGdeQXVyMTM0NTUzNDIy._V1_.jpg",
-            },
-            {
-                name:"Jumanji",
-                rating:7.1,
-                cover:"https://m.media-amazon.com/images/S/pv-target-images/6605eddeb3e44a153694c675cd1129d2a9d7d25576366657536f7311d9d7dc99.jpg",
-            },
-            {
-                name:"Spider Man",
-                rating:7.4,
-                cover:"https://moviesmedia.ign.com/movies/image/object/033/033717/spider-man_poster.jpg",
-            },
-            {
-                name:"Avatar",
-                rating:7.9,
-                cover:"https://lumiere-a.akamaihd.net/v1/images/avatar_800x1200_208c9665.jpeg?region=0%2C0%2C800%2C1200",
+                cast:"Robert Downey. Chris Hemsworth . Scarlett Johansonn"
             },
             {
                 name:"Dune",
                 rating:8.0,
                 cover:"https://m.media-amazon.com/images/M/MV5BMDQ0NjgyN2YtNWViNS00YjA3LTkxNDktYzFkZTExZGMxZDkxXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_FMjpg_UX1000_.jpg",
+                cast: "Timothée Chalamet . Zendaya . Oscar Isaac"
+            },
+
+            {
+                name:"Avatar",
+                rating:7.9,
+                cover:"https://lumiere-a.akamaihd.net/v1/images/avatar_800x1200_208c9665.jpeg?region=0%2C0%2C800%2C1200",
+                cast: "Zoe Saldana. Sam Worthington . Stephen Lang"
+            },
+            
+            {
+                name:"The Dark Knight",
+                rating:9,
+                cover:"https://contentserver.com.au/assets/598411_p173378_p_v8_au.jpg",
+                cast:"Christian Bale. Heath Ledger . Morgan Freeman"
             },
             {
-                name:"Godzilla",
-                rating:6.0,
-                cover:"https://m.media-amazon.com/images/M/MV5BOGFjYWNkMTMtMTg1ZC00Y2I4LTg0ZTYtN2ZlMzI4MGQwNzg4XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg",
+                name:"Spider Man",
+                rating:7.4,
+                cover:"https://moviesmedia.ign.com/movies/image/object/033/033717/spider-man_poster.jpg",
+                cast: "Tobey Maguire. Kirsten Dunst . Willem Dafoe"
             },
+
+
             {
                 name:"The conjuring",
                 rating:7.5,
                 cover:"https://m.media-amazon.com/images/M/MV5BMTM3NjA1NDMyMV5BMl5BanBnXkFtZTcwMDQzNDMzOQ@@._V1_.jpg",
+                cast: "Vera Farmiga . Patrick Wilson . Sterling Jerins"
             },
             {
                 name:"Free Guy",
                 rating:7.1,
                 cover:"https://m.media-amazon.com/images/M/MV5BOTY2NzFjODctOWUzMC00MGZhLTlhNjMtM2Y2ODBiNGY1ZWRiXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg",
+                cast: "Ryan Reynolds . Jodie Comer . Joe Keery"
+            },
+            {
+                name: "Five Feet Apart",
+                rating: 7.2,
+                cover: "https://m.media-amazon.com/images/M/MV5BNzVmMjJlN2MtNWQ4Ny00Zjc2LWJjYTgtYjJiNGM5MTM1ZTlkXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_.jpg",
+                cast: "Haley Lu Richardson, Cole Sprouse, Moises Arias"
+            },
+            {
+                name: "The Incredible Hulk",
+                rating: 6.7,
+                cover: "https://mir-s3-cdn-cf.behance.net/project_modules/hd/c9cc26104258611.5f5f69fcc5dce.jpg",
+                cast: "Edward Norton, Liv Tyler, Tim Roth"
+            },
+
+            {
+                name:"Godzilla",
+                rating:6.0,
+                cover:"https://m.media-amazon.com/images/M/MV5BOGFjYWNkMTMtMTg1ZC00Y2I4LTg0ZTYtN2ZlMzI4MGQwNzg4XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg",
+                cast: "Millie Bobby Brown . Kyle Chandler . Sally Hawkins"
+            }, 
+
+            
+
+            {
+                name:"Venom",
+                rating:6.6,
+                cover:"https://m.media-amazon.com/images/M/MV5BNTFkZjdjN2QtOGE5MS00ZTgzLTgxZjAtYzkyZWQ5MjEzYmZjXkEyXkFqcGdeQXVyMTM0NTUzNDIy._V1_.jpg",
+                cast:"Tom Hardy. Michelle Williams . Riz Ahmed"
+            },
+            {
+                name: "The Matrix",
+                rating: 8.7,
+                cover: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+                cast: "Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss"
+            },
+            {
+                name: "Lord of the Rings",
+                rating: 8.9,
+                cover: "https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781608873821/the-lord-of-the-rings-9781608873821_hr.jpg",
+                cast: "Elijah Wood, Ian McKellen, Orlando Bloom"
+            },
+            {
+                name: "Titanic",
+                rating: 7.8,
+                cover: "https://a.ltrbxd.com/resized/film-poster/5/1/5/2/4/51524-titanic-0-230-0-345-crop.jpg?v=7517ea94ce",
+                cast: "Leonardo DiCaprio, Kate Winslet, Billy Zane"
             },
             
+                      
+
+            {
+                name: "Fast & Furious",
+                rating: 6.8,
+                cover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTupoL5bu1KP9miI9yfPG0ea5n6-dR5NBycSGmVhJgrXA&s",
+                cast: "Vin Diesel, Paul Walker, Michelle Rodriguez"
+            },
+            {
+                name: "Ghost Rider",
+                rating: 5.2,
+                cover: "https://i.pinimg.com/736x/4b/22/8f/4b228fc6f2674f7eb7c48af2c6cbf323.jpg",
+                cast: "Nicolas Cage, Eva Mendes, Sam Elliott"
+            },
+            {
+                name: "Drunken Master",
+                rating: 7.5,
+                cover: "https://m.media-amazon.com/images/I/51cJs1DMlJL._AC_UF894,1000_QL80_.jpg",
+                cast: "Jackie Chan, Siu Tin Yuen, Jang Lee Hwang"
+            },
+            {
+                name: "Me Before You",
+                rating: 7.4,
+                cover: "https://m.media-amazon.com/images/I/81fTRhi9E+L._AC_UF1000,1000_QL80_.jpg",
+                cast: "Emilia Clarke, Sam Claflin, Janet McTeer"
+            },
+
+            {
+                name:"Jumanji",
+                rating:7.1,
+                cover:"https://m.media-amazon.com/images/S/pv-target-images/6605eddeb3e44a153694c675cd1129d2a9d7d25576366657536f7311d9d7dc99.jpg",
+                cast: "Robin Williams . Kirsten Dunst . Bradley Pierce"
+            },
+
+            {
+                name: "The Suicide Squad",
+                rating: 7.3,
+                cover: "https://upload.wikimedia.org/wikipedia/en/5/5c/Suicide_Squad_%282016_film%29_poster.png",
+                cast: "Margot Robbie, Idris Elba, John Cena"
+            },
+
+            
+        
 
         ];
 
@@ -96,7 +181,7 @@ function displayMovies(data)
                     <div class="details">
                         <h1>${data[i].name}</h1>
                         <h2>IMDB : ${data[i].rating}</h2>
-                        <p>Robert Downey. Chris Hemsworth . Scarlett Johansonn</p>
+                        <p>${data[i].cast}</p>
 
                     </div>
                 </div>
